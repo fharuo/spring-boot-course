d1888e54ce2a155e1b8b6fe4053e79b93ecb03e9
Fabricio <fabriciohtakaki@gmail.com>
2021-12-17 14:43:15 -0300

    initial commit

---
+++ src/main/java/org/fabriciotakaki/VendasApplication.java
@@ -0,0 +1,12 @@
+package org.fabriciotakaki;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+
+    }
+}
